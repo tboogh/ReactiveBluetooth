5a9f4a0948d068fea8bcbfa276f79043b86dd78a
Tobias Boogh <t@boogh.se>
2016-11-12 22:59:13 +0100

    Added xunit

--- IntegrationsTests/ReactiveBluetooth.Shared.IntegrationsTests/CentralTests.cs
+++ IntegrationsTests/ReactiveBluetooth.Shared.IntegrationsTests/CentralTests.cs
@@ -5,15 +5,14 @@ using System.Reactive.Linq;
 using System.Reactive.Threading.Tasks;
 using System.Threading;
 using System.Threading.Tasks;
-using NUnit;
-using NUnit.Framework;
 using ReactiveBluetooth.Core;
 using ReactiveBluetooth.Core.Central;
 using ReactiveBluetooth.Core.Types;
+using Xunit;
 
 namespace ReactiveBluetooth.Shared.IntegrationsTests
 {
-    [TestFixture]
+    
     public abstract class CentralTests
     {
         private static readonly TimeSpan Timeout = TimeSpan.FromSeconds(10);
@@ -21,7 +20,7 @@ namespace ReactiveBluetooth.Shared.IntegrationsTests
         private ICentralManager _centralManager;
         public abstract ICentralManager GetCentralManager();
 
-        [OneTimeSetUp]
+        
         public async Task SetupManager()
         {
             _centralManager = GetCentralManager();
@@ -30,30 +29,30 @@ namespace ReactiveBluetooth.Shared.IntegrationsTests
                 .Timeout(Timeout);
         }
 
-        [Test]
+        [Fact]
         public async Task ManagerState_IsPoweredOn()
         {
             var currentState = await _centralManager.State()
                 .FirstAsync(state => state == ManagerState.PoweredOn)
                 .Timeout(Timeout);
-            Assert.AreEqual(ManagerState.PoweredOn, currentState);
+            Assert.Equal(ManagerState.PoweredOn, currentState);
         }
 
-        [Test]
+        [Fact]
         public async Task ScanForDevices_DiscoversDevicesFindAny_NotNull()
         {
             var device = await _centralManager.ScanForDevices(new List<Guid>() { Guid.Parse("B0060000-0234-49D9-8439-39100D7EBD62") }).FirstAsync().Timeout(Timeout);
             Assert.NotNull(device);
         }
 
-        [Test]
+        [Fact]
         public async Task ScanForDevices_DiscoversDevicesServiceFilterFindAny_NotNull()
         {
             var device = await _centralManager.ScanForDevices().FirstAsync().Timeout(Timeout);
             Assert.NotNull(device);
         }
 
-        [Test]
+        [Fact]
         public async Task ScanForDevices_FindTestDevice_NotNull()
         {
             var scanObservable = _centralManager.ScanForDevices(new List<Guid>() { Guid.Parse("B0060000-0234-49D9-8439-39100D7EBD62") });
@@ -62,7 +61,7 @@ namespace ReactiveBluetooth.Shared.IntegrationsTests
             Assert.NotNull(testDevice);
         }
 
-        [Test]
+        [Fact]
         public async Task ConnectToDevice_ConnectToTestDevice_ResultIsTrue()
         {
             var scanObservable = _centralManager.ScanForDevices(new List<Guid>() { Guid.Parse("B0060000-0234-49D9-8439-39100D7EBD62") });
@@ -78,10 +77,10 @@ namespace ReactiveBluetooth.Shared.IntegrationsTests
             CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(Timeout);
             await _centralManager.Disconnect(device, cancellationTokenSource.Token);
 
-            Assert.AreEqual(ConnectionState.Connected, connectionResult);
+            Assert.Equal(ConnectionState.Connected, connectionResult);
         }
 
-        [Test]
+        [Fact]
         public async Task ConnectToDevice_ConnectToTestDevice_StateIsConnected()
         {
             var device = await FindTestDevice();
@@ -90,10 +89,10 @@ namespace ReactiveBluetooth.Shared.IntegrationsTests
             CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(Timeout);
             await _centralManager.Disconnect(device, cancellationTokenSource.Token);
 
-            Assert.AreEqual(ConnectionState.Connected, state);
+            Assert.Equal(ConnectionState.Connected, state);
         }
 
-        [Test]
+        [Fact]
         public async Task DisconnectDevice_DisconnectFromTestDevice_StateIsDisconnected()
         {
             var device = await ConnectToTestDevice();
@@ -101,10 +100,10 @@ namespace ReactiveBluetooth.Shared.IntegrationsTests
             CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(Timeout);
             await _centralManager.Disconnect(device, cancellationTokenSource.Token);
             
-            Assert.AreEqual(ConnectionState.Disconnected, device.State);
+            Assert.Equal(ConnectionState.Disconnected, device.State);
         }
 
-        [Test]
+        [Fact]
         public async Task DiscoverServices_FindServicesOnTestDevice_ListNotNull()
         {
             var device = await ConnectToTestDevice();
@@ -115,10 +114,10 @@ namespace ReactiveBluetooth.Shared.IntegrationsTests
             cancellationTokenSource.CancelAfter(Timeout);
             await _centralManager.Disconnect(device, cancellationTokenSource.Token);
             Assert.NotNull(services);
-            Assert.IsNotEmpty(services);
+            Assert.NotEmpty(services);
         }
 
-        [Test]
+        [Fact]
         public async Task DiscoverServices_FindServicesOnTestDevice_ServiceIdSet()
         {
             var device = await ConnectToTestDevice();
@@ -140,7 +139,7 @@ namespace ReactiveBluetooth.Shared.IntegrationsTests
             Assert.NotNull(testService?.Uuid);
         }
 
-        [Test]
+        [Fact]
         public async Task DiscoverCharacteristics_FindCharacteristicsOnService_NotNullList()
         {
             var device = await ConnectToTestDevice();
@@ -162,7 +161,7 @@ namespace ReactiveBluetooth.Shared.IntegrationsTests
             Assert.NotNull(testCharacterstic);
         }
 
-        [Test]
+        [Fact]
         public async Task ReadValue_CorrectValue()
         {
             var device = await ConnectToTestDevice();
@@ -183,10 +182,10 @@ namespace ReactiveBluetooth.Shared.IntegrationsTests
 
             cancellationTokenSource.CancelAfter(Timeout);
             await _centralManager.Disconnect(device, cancellationTokenSource.Token);
-            Assert.AreEqual(new byte[] { 0xB0, 0x06, 0x00, 0x01 }, value);
+            Assert.Equal(new byte[] { 0xB0, 0x06, 0x00, 0x01 }, value);
         }
 
-        [Test]
+        [Fact]
         public async Task WriteValue_WithResponse()
         {
             var device = await ConnectToTestDevice();
@@ -208,10 +207,10 @@ namespace ReactiveBluetooth.Shared.IntegrationsTests
 
             cancellationTokenSource.CancelAfter(Timeout);
             await _centralManager.Disconnect(device, cancellationTokenSource.Token);
-            Assert.IsTrue(success);
+            Assert.True(success);
         }
 
-        [Test]
+        [Fact]
         public async Task WriteValue_WithoutResponse()
         {
             var device = await ConnectToTestDevice();
@@ -233,10 +232,10 @@ namespace ReactiveBluetooth.Shared.IntegrationsTests
 
             cancellationTokenSource.CancelAfter(Timeout);
             await _centralManager.Disconnect(device, cancellationTokenSource.Token);
-            Assert.IsTrue(success);
+            Assert.True(success);
         }
 
-        [Test]
+        [Fact]
         public async Task WriteValue_WithResponse_CorrectValue()
         {
             var device = await ConnectToTestDevice();
@@ -261,10 +260,10 @@ namespace ReactiveBluetooth.Shared.IntegrationsTests
 
             cancellationTokenSource.CancelAfter(Timeout);
             await _centralManager.Disconnect(device, cancellationTokenSource.Token);
-            Assert.AreEqual(new byte[] { 0x12, 0x34, 0x56, 0x78 }, value);
+            Assert.Equal(new byte[] { 0x12, 0x34, 0x56, 0x78 }, value);
         }
 
-        [Test]
+        [Fact]
         public async Task WriteValue_WithoutResponse_CorrectValue()
         {
             var device = await ConnectToTestDevice();
@@ -290,7 +289,7 @@ namespace ReactiveBluetooth.Shared.IntegrationsTests
             var value = await device.ReadValue(writeCharacterstic, cancellationTokenSource.Token);
             cancellationTokenSource.CancelAfter(TimeSpan.FromSeconds(30));
             await _centralManager.Disconnect(device, cancellationTokenSource.Token);
-            Assert.AreEqual(new byte[] { 0x12, 0x34, 0x56, 0x78 }, value);
+            Assert.Equal(new byte[] { 0x12, 0x34, 0x56, 0x78 }, value);
         }
 
         private async Task<IDevice> ConnectToTestDevice()
